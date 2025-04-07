d11f32232a97e00d205c93259dc56e3ef3090e8f
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-04-07 21:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
   // your code here
+	let ans = "";
+	for(let ch of s) {
+		if(ch!==" ") {
+			ans += ch;
+		}
+		else {
+			if(ans.length!==0) break;
+		}
+	}
+
+	return ans;
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
